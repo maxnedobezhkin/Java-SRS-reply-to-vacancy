2028b78c3f00e63dab4d0feb258d52b17a0f8d0a
max <firkis@yandex.ru>
2022-05-23 20:31:07 +0700

    Создал проект

---
+++ reply-to-vacansy/src/main/java/com/example/replytovacansy/ReplyToVacansyApplication.java
@@ -0,0 +1,13 @@
+package com.example.replytovacansy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplyToVacansyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplyToVacansyApplication.class, args);
+	}
+
+}
---
+++ reply-to-vacansy/src/test/java/com/example/replytovacansy/ReplyToVacansyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.replytovacansy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplyToVacansyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
